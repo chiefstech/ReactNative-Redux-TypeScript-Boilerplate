aae89faffdaeedcefd9bf1e08754639a2615a952
Nelson Omuto <nelsonomuto@gmail.com>
2020-06-28 18:04:33 -0400

    wip

---
+++ ios/Pods/Target Support Files/Pods-RNReduxSeed-tvOSTests/Pods-RNReduxSeed-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNReduxSeed_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_RNReduxSeed_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-RNReduxSeedTests/Pods-RNReduxSeedTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNReduxSeedTests : NSObject
+@end
+@implementation PodsDummy_Pods_RNReduxSeedTests
+@end
